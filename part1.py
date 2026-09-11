c07a47caa1c0c496d13da8c98d73746dcee9416f
Andrew Hoang <andrewhoang111@gmail.com>
2026-09-10 19:30:07 -0500

    updated data preprocessing in part1.py
    Added data preprocessing steps including resetting index and feature scaling.

--- part1.py
+++ part1.py
@@ -1,7 +1,9 @@
-from ucimlrepo import fetch_ucirepo
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
+from ucimlrepo import fetch_ucirepo 
   
 # fetch dataset 
 auto_mpg = fetch_ucirepo(id=9) 
@@ -9,18 +11,24 @@ auto_mpg = fetch_ucirepo(id=9)
 # data (as pandas dataframes) 
 X = auto_mpg.data.features 
 y = auto_mpg.data.targets 
-  
-# Combine features and target into a single dataframe
+
 df = pd.concat([X, y], axis=1)
 
+df = df.dropna()  # Drop rows with missing values
+df = df.drop_duplicates()  # Drop duplicate rows
+df = df.reset_index(drop=True)  # Reset index after dropping rows
 
-# Remove null/NA rows
-df = df.dropna()
+X = df.drop(columns=['mpg'])
+y = df['mpg']
 
-# Remove duplicate rows
-df = df.drop_duplicates()
-
-# Display the first few rows of the dataframe
-print(df.columns.tolist())
+scalar = StandardScaler()
+X = scalar.fit_transform(X)
 
 
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print(y_train)
